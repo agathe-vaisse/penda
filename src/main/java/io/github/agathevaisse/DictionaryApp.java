e5d80d40c88f3b47e18d92ec168b4a07a7c2ba05
Florent Biville <florent.biville@gmail.com>
2020-10-07 18:51:03 +0200

    Effectively make HTTP client a singleton

--- src/main/java/io/github/agathevaisse/DictionaryApp.java
+++ src/main/java/io/github/agathevaisse/DictionaryApp.java
@@ -19,41 +19,37 @@ public class DictionaryApp {
 
     public static final String API_ROOT_PATH = "/api";
 
-    // TODO: "singleton-ize" lexicalaHttpClient
     // TODO: introduce Either<Error, X> on XxxApi method return types and adapt Visitor
     public static void main(String[] args) {
         SimpleEnvironment environment = new SimpleEnvironment(fromClasspathResource("/application.properties"));
         int port = environment.getOrDefault("port", Integer::parseInt, 8080);
-        LanguageApi languageApi = languageApi(environment);
-        WordApi wordApi = wordApi(environment);
+        HttpClient httpClient = lexicalaHttpClient(environment);
+        LanguageApi languageApi = languageApi(languageRepository(environment, httpClient));
+        WordApi wordApi = wordApi(wordRepository(environment, httpClient));
         Spark.port(port);
         Spark.get(LanguageApi.ALL_LANGUAGES_PATH, languageApi::getAllLanguages, LanguageJsonVisitor::visitAll);
         Spark.post(WordApi.RANDOM_WORD_PATH, wordApi::postRandomWord, WordJsonVisitor::visit);
     }
 
-    private static LanguageApi languageApi(SimpleEnvironment environment) {
-        return new LanguageApi(
-            languageRepository(environment)
-        );
+    private static LanguageApi languageApi(LanguageRepository languageRepository) {
+        return new LanguageApi(languageRepository);
     }
 
-    private static WordApi wordApi(SimpleEnvironment environment) {
-        return new WordApi(
-            wordRepository(environment)
-        );
+    private static WordApi wordApi(WordRepository wordRepository) {
+        return new WordApi(wordRepository);
     }
 
-    private static LanguageRepository languageRepository(SimpleEnvironment environment) {
+    private static LanguageRepository languageRepository(SimpleEnvironment environment, HttpClient httpClient) {
         return new LanguageRepository(
             environment.get("lexicala.base-uri"),
-            lexicalaHttpClient(environment)
+            httpClient
         );
     }
 
-    private static WordRepository wordRepository(SimpleEnvironment environment) {
+    private static WordRepository wordRepository(SimpleEnvironment environment, HttpClient httpClient) {
         return new WordRepository(
             environment.get("lexicala.base-uri"),
-            lexicalaHttpClient(environment)
+            httpClient
         );
     }
 
