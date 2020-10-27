5da3d24cd62b78d9f4bf342cc60f2a8a3eeb6f20
Agathe Vaisse <25689895+agathe-vaisse@users.noreply.github.com>
2020-10-27 20:14:09 +0100

    Reset game form after each attempt
    Fixes #13

--- penda/src/app/game/game.component.spec.ts
+++ penda/src/app/game/game.component.spec.ts
@@ -18,6 +18,13 @@ describe('GameComponent', () => {
     const wordToGuess = {value: 'panda'} as Word;
     let initialGameState: GameState;
 
+    const submitGameForm = (attempt: string) => {
+        const attemptInput = dom.querySelector('#letter');
+        attemptInput.value = attempt;
+        attemptInput.dispatchEvent(new Event('input'));
+        dom.querySelector('#try').click();
+    };
+
     beforeEach(async () => {
         initialGameState = {
             wordState: {
@@ -73,11 +80,16 @@ describe('GameComponent', () => {
             done();
         });
 
-        const attemptInput = dom.querySelector('#letter');
-        attemptInput.value = attempt;
-        attemptInput.dispatchEvent(new Event('input'));
-        dom.querySelector('#try').click();
+        submitGameForm(attempt);
         fixture.detectChanges();
+    });
+
+
+    it('should clear input after each attempt', () => {
+        submitGameForm('c');
+        fixture.detectChanges();
+
+        expect(dom.querySelector('#letter').value).toEqual('');
     });
 
     // TODO: find out why subscription is already closed
